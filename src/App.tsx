3de4e46437ea9e2456bedface636a6439e774afa
Hongbin Li <hblee12294@gmail.com>
2019-02-20 08:36:12 -0800

    chore: eject and archieve

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 
+// import ThreeContainer from './Three/ThreeContainer'
+// import Test from './Three/Test'
+import Test2 from './Three/Test2'
+
 class App extends Component {
   public render() {
     return (
       <div className="App">
+        { /* <ThreeContainer /> */ }
+        <Test2 />
       </div>
     )
   }
--- src/Three/Test2.tsx
+++ src/Three/Test2.tsx
@@ -4,12 +4,14 @@ import data from '../data/Zaghetto.pcd'
 
 declare var THREE: any
 
-class ThreeContainer extends Component {
+class Test extends Component {
   private mount: any
   private scene: any
   private camera: any
   private renderer: any
   private frameId: any
+  private material: any
+  private cube: any
 
   public componentDidMount() {
     const width = this.mount.clientWidth
@@ -18,36 +20,47 @@ class ThreeContainer extends Component {
     const scene = new THREE.Scene()
     scene.background = new THREE.Color(0x000000)
 
-    console.log(width / height)
-    const camera = new THREE.PerspectiveCamera(15, width / height, 0.01, 40)
+    const camera = new THREE.PerspectiveCamera(
+      15,
+      width / height,
+      0.01,
+      40,
+    )
     camera.position.x = 0.4
     camera.position.z = -2
     camera.up.set(0, 0, 1)
-    console.log(camera)
 
     scene.add(camera)
 
     const renderer = new THREE.WebGLRenderer({ antialias: true })
-    // renderer.setClearColor('#000000')
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(width, height)
-
-    this.camera = camera
-    this.renderer = renderer
-
-    this.mount.appendChild(this.renderer.domElement)
-
+    // const geometry = new THREE.BoxGeometry(1, 1, 1)
+    // const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
+    // const cube = new THREE.Mesh(geometry, material)
+    // console.log(cube)
     const loader = new THREE.PCDLoader()
     loader.load(data, (mesh: any) => {
-      scene.add(mesh)
-      console.log(mesh)
       console.log(data)
-      // const center = mesh.geometry.boundingSphere.center;
+      console.log(mesh)
+      scene.add(mesh)
+      // const center = mesh.geometry.boundingSphere.center
+      // controls.target.set(center.x, center.y, center.z)
+      // controls.update()
     })
 
-    this.scene = scene
-    console.log(this.scene)
+    // camera.position.z = 4
+    // scene.add(cube)
+    // renderer.setClearColor('#000000')
+    // renderer.setSize(width, height)
 
+    this.scene = scene
+    this.camera = camera
+    this.renderer = renderer
+    // this.material = material
+    // this.cube = cube
+
+    this.mount.appendChild(this.renderer.domElement)
     this.start()
   }
 
@@ -66,23 +79,26 @@ class ThreeContainer extends Component {
     cancelAnimationFrame(this.frameId)
   }
 
-  public renderScene = () => {
-    this.renderer.render(this.scene, this.camera)
-  }
-
   public animate = () => {
+    // this.cube.rotation.x += 0.01
+    // this.cube.rotation.y += 0.01
+
     this.renderScene()
     this.frameId = window.requestAnimationFrame(this.animate)
+  }
+
+  public renderScene = () => {
+    this.renderer.render(this.scene, this.camera)
   }
 
   public render() {
     return (
       <div
-        style={ { width: '1000px', height: '800px' } }
-        ref={ (mount) => (this.mount = mount) }
+        style={ { width: '800px', height: '800px' } }
+        ref={ (mount) => { this.mount = mount} }
       />
     )
   }
 }
 
-export default ThreeContainer
+export default Test
